831a013fce057beeddcd9ecc5a05d10b8dc0df88
mukulrocque <mukulrocque@gmail.com>
2014-11-23 14:12:34 +0100

    assign2 completed after fork

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,22 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## R function to cache potentially time consuming operations
+## using superassignment variable <<- to understand lexical scoping
+## Caching the inverse of a MAtrix
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        ## function to create an object
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 
 }
 
@@ -12,4 +25,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        ## checks if inverse is present (also if matrix changed m <- NULL)
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        ## executed only if m == NULL implies matrix changed or inverse wasn't present
+        data <- x$get()
+        ##compute inverse
+        m <- solve(data, ...)           
+        ## set inverse using super assignment
+        x$setinverse(m)
+        ## return calculated inverse
+        m
 }
