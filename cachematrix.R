34f78642ea7e011e3a45a7250f9ff10f080db741
mukulrocque <mukulrocque@gmail.com>
2014-11-23 14:26:22 +0100

    assign2 completed after fork

--- cachematrix.R
+++ cachematrix.R
@@ -5,24 +5,26 @@
 ## Caching the inverse of a MAtrix
 
 ## Write a short comment describing this function
-
+## creates special matrix opbject that can cache ints inverse
 makeCacheMatrix <- function(x = matrix()) {
-        ## function to create an object
         m <- NULL
         set <- function(y) {
                 x <<- y
                 m <<- NULL
         }
         get <- function() x
+        ## sets inverse into m
         setinverse <- function(inverse) m <<- inverse
+        ## retrieves inverse matrix
         getinverse <- function() m
+        ## returned list to allow usage of functions
         list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 
 }
 
 
 ## Write a short comment describing this function
-
+## computes inverse of special matrix if the matrix has changed else returns cached inverse
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         m <- x$getinverse()
